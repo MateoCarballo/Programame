adecf6158e47e4c6cdea2bc867819cb5609ee98b
Ruben Baqueiro <baqueiroruben@gmail.com>
2023-04-05 12:07:15 +0200

    Correciones Abuela Maria y Artrópodos

--- Pruebas_Programame/src/Semana2/LaAbuelaMaria.java
+++ Pruebas_Programame/src/Semana2/LaAbuelaMaria.java
@@ -1,33 +1,26 @@
 package Pruebas_Programame.src.Semana2;
 
+//https://www.aceptaelreto.com/problem/statement.php?id=337
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class LaAbuelaMaria {
+public class LaAbuelaMaria {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int casos = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < casos; i++) {
-
-            String[] entrada1 = new String[6];
-            String[] entrada2 = new String[6];
-
-            entrada1 = br.readLine().split("\\s+");
-            entrada2 = br.readLine().split("\\s+");
-
-            for (int j = 0; j < entrada1.length; j++) {
-
-                for (int j2 = entrada2.length - 1; j2 >= 0; j2--) {
-
-                    if (Integer.parseInt(entrada1[j]) == Integer.parseInt(entrada2[j2])) {
-                        System.out.println("si");
-                    }
+            String[] entrada1 = br.readLine().split(" ");
+            String[] entrada2 = br.readLine().split(" ");
+            int z = Integer.parseInt(entrada1[0]) + Integer.parseInt(entrada2[0]);
+            for (int x = 0; x < entrada1.length; x++) {
+                if (Integer.parseInt(entrada1[x]) + Integer.parseInt(entrada2[x]) != z) {
+                    System.out.println("NO");
+                    break;
+                } else if (x == (entrada1.length - 1)) {
+                    System.out.println("SI");
                 }
             }
-
         }
     }
-
 }
