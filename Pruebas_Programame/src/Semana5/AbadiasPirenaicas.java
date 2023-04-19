e91f64da770202571094e2cbc9e82b96dd6e50da
Puffito <puffitored@gmail.com>
2023-04-19 20:58:14 +0200

    Base de abadia y Pollos

--- Pruebas_Programame/src/Semana5/AbadiasPirenaicas.java
+++ Pruebas_Programame/src/Semana5/AbadiasPirenaicas.java
@@ -1,5 +1,21 @@
 package Semana5;
 
-public class AbadiasPirenaicas {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class AbadiasPirenaicas {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String entrada = br.readLine();
+            if(entrada.equals("0")){break;}
+            String[] datos = entrada.split(" ");
+            int contador = 0;
+            for(int x = 1;x==Integer.parseInt(datos[0]);x++){
+
+            }
+
+        }
+    }
 }
--- Pruebas_Programame/src/Semana5/LaComidadeLosPollitos.java
+++ Pruebas_Programame/src/Semana5/LaComidadeLosPollitos.java
@@ -1,5 +1,23 @@
 package Semana5;
 
-public class LaComidadeLosPollitos {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class LaComidadeLosPollitos {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int casos = Integer.parseInt(br.readLine());
+        for (int i = 0; i < casos; i++) {
+            String[] areaPollos = br.readLine().split(" ");
+            int[][] corral = new int[Integer.parseInt(areaPollos[0])][Integer.parseInt(areaPollos[1])];
+            for (int j = 0 ; j < Integer.parseInt(areaPollos[2]); j++){
+                String[] polloInfo = br.readLine().split(" ");
+                corral[Integer.parseInt(polloInfo[0])][Integer.parseInt(polloInfo[1])] +=1;
+                for (int x = 0 ; x < Integer.parseInt(polloInfo[3]); x++){
+
+                }
+            }
+        }
+    }
 }
