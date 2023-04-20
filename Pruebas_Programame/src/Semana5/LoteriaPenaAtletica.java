bcbf735d93ab1e2ee9fc7225a54e7a60268f0ebb
Puffito <puffitored@gmail.com>
2023-04-20 09:47:11 +0200

    Loteria sin arreglar

--- Pruebas_Programame/src/Semana5/LoteriaPenaAtletica.java
+++ Pruebas_Programame/src/Semana5/LoteriaPenaAtletica.java
@@ -4,15 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class LoteriaPeñaAtletica {
+public class LoteriaPenaAtletica {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int casos = Integer.parseInt(br.readLine());
         for (int i = 0; i < casos; i++) {
-            int[] numeros = new int[Integer.parseInt(br.readLine())];
+            br.readLine();
             int contador= 0 ;
-            for (int numero: numeros) {
-                if (numero % 2 == 0){contador++;}
+            String[] numeros = br.readLine().split(" ");
+            for (String numero: numeros) {
+                if (Integer.parseInt(numero) % 2 == 0){contador++;}
             }
             System.out.println(contador);
         }
