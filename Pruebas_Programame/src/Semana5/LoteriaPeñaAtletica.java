6dfa6610ea84583f7f214b26c858c19a4fff2772
Puffito <puffitored@gmail.com>
2023-04-20 09:40:39 +0200

    Arreglado la estructura del proyecto 2

--- Pruebas_Programame/src/Semana5/LoteriaPeñaAtletica.java
+++ Pruebas_Programame/src/Semana5/LoteriaPeñaAtletica.java
@@ -1,5 +1,20 @@
 package Semana5;
 
-public class LoteriaPeñaAtletica {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class LoteriaPeñaAtletica {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int casos = Integer.parseInt(br.readLine());
+        for (int i = 0; i < casos; i++) {
+            int[] numeros = new int[Integer.parseInt(br.readLine())];
+            int contador= 0 ;
+            for (int numero: numeros) {
+                if (numero % 2 == 0){contador++;}
+            }
+            System.out.println(contador);
+        }
+    }
 }
